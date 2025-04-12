669123c9cdde3c1d56ef33bea2b12f4df4bb621b
Meykaa <meghaganesan73@gmail.com>
2025-04-13 00:18:10 +0530

    🚀 Added Docker support and backend restructuring

--- backend/app.py
+++ backend/app.py
@@ -147,4 +147,6 @@ if __name__ == "__main__":
     for rule in app.url_map.iter_rules():
         print(rule)
 
-    app.run(debug=False, threaded=True)
+    app.run(host="0.0.0.0", port=5000, debug=False, threaded=True)
+
+
